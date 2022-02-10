dd950b195eda3d8518e3d65c91007729d35833bf
justineholland <justineholland2@gmail.com>
2022-02-10 13:34:25 -0600

    index route finished)

--- resources/pantrys.py
+++ resources/pantrys.py
@@ -1,14 +1,17 @@
 import models
 
-from flask import Blueprint, request
-
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
 
 
 pantrys= Blueprint('pantrys', 'pantrys')
 
 @pantrys.route('/', methods=['GET'])
 def pantrys_index():
-    return "pantry is working"
+    result = models.Pantry.select()
+    print('results of pantry')
+    print(result)
+    return "in terminal "
 
 
 @pantrys.route('/', methods=['POST'])
@@ -17,4 +20,13 @@ def create_pantry():
     print(payload)
     new_pantry = models.Pantry.create(item=payload['item'], quantity=payload['quantity'], category=payload['category'])
     print(new_pantry)
-    return "created a new item"
+    print(new_pantry.__dict__)
+
+
+    pantry_dict = model_to_dict(new_pantry)
+
+    return jsonify(
+    data=pantry_dict,
+    message='Successfully created a new item in your pantry',
+    status=201
+    ),201
