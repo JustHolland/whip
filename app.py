4493fca5bb6c8b2ca9af17fdc3fb74bcd59d0b80
justineholland <justineholland2@gmail.com>
2022-02-10 11:21:25 -0600

    DB connects

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 
+import models
+
 DEBUG = True
 PORT = 8000
 
@@ -7,15 +9,28 @@ PORT = 8000
 # This starts the website!
 app = Flask(__name__)
 
+
 # The default URL ends in / ("my-website.com/").
 @app.route('/')
 def index():
     return 'Welcome to Whip.'
 
+@app.route('/nested_json')
+def get_nested_json():
+    food_dict = {
+        'item': 'chicken',
+        'quanity': 2,
+
+    }
+    return jsonify(name="Justine", age=29, food=food_dict)
+
+
+
 @app.route('/pantry/<username>') # When someone goes here...
 def hello(username): # Do this.
-    return " {}'s Pantry".format(username)
+    return f" {username}'s Pantry"
 
 # Run the app when the program starts!
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
---
+++ models.py
@@ -0,0 +1,19 @@
+from peewee import *
+import datetime
+
+DATABASE = SqliteDatabase('whip.sqlite')
+
+
+class Pantry(Model):
+    item = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Pantry], safe=True)
+    print("TABLES Created")
+    DATABASE.close()
