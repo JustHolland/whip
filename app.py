89a5a29f2ed5098329ed3784252c5001fe8b0b36
justineholland <justineholland2@gmail.com>
2022-02-14 13:32:13 -0600

    user relationsip createdd

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, jsonify
 
 from resources.pantrys import pantrys
+from resources.users import users
 
 import models
 from flask_cors import CORS
+from flask_login import LoginManager
+
 
 DEBUG = True
 PORT = 8000
 
-# Initialize an instance of the Flask class.
-# This starts the website!
+
 app = Flask(__name__)
+app.secret_key = "ALGERNON"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 
 CORS(pantrys, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
+
 
 app.register_blueprint(pantrys, url_prefix='/api/v1/pantrys')
+app.register_blueprint(users, url_prefix='/api/v1/users')
 # The default URL ends in / ("my-website.com/").
 @app.route('/')
 def index():
--- models.py
+++ models.py
@@ -1,13 +1,23 @@
 from peewee import *
 import datetime
-
+from flask_login import UserMixin
 DATABASE = SqliteDatabase('whip.sqlite')
+
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
 
 
 class Pantry(Model):
     item = CharField()
     quantity = IntegerField()
     category = CharField()
+    user = ForeignKeyField(User, backref = 'pantrys')
     created_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
@@ -16,6 +26,6 @@ class Pantry(Model):
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([Pantry], safe=True)
+    DATABASE.create_tables([User, Pantry], safe=True)
     print("TABLES Created")
     DATABASE.close()
